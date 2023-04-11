6d251d89117dc34acd3a3f5e3ef4384fa620c171
Dzmitry Salodki <dsalodki@gmail.com>
2023-04-11 14:24:36 +0300

    Add project files.

---
+++ Crypto/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Crypto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbBinance = new System.Windows.Forms.TextBox();
+            this.tbBybit = new System.Windows.Forms.TextBox();
+            this.tbKucoin = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Binance";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bybit";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kucoin";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbKucoin);
+            this.groupBox1.Controls.Add(this.tbBybit);
+            this.groupBox1.Controls.Add(this.tbBinance);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(284, 180);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "BTCUSDT";
+            // 
+            // tbBinance
+            // 
+            this.tbBinance.Location = new System.Drawing.Point(95, 16);
+            this.tbBinance.Name = "tbBinance";
+            this.tbBinance.Size = new System.Drawing.Size(100, 26);
+            this.tbBinance.TabIndex = 3;
+            // 
+            // tbBybit
+            // 
+            this.tbBybit.Location = new System.Drawing.Point(95, 48);
+            this.tbBybit.Name = "tbBybit";
+            this.tbBybit.Size = new System.Drawing.Size(100, 26);
+            this.tbBybit.TabIndex = 4;
+            // 
+            // tbKucoin
+            // 
+            this.tbKucoin.Location = new System.Drawing.Point(95, 81);
+            this.tbKucoin.Name = "tbKucoin";
+            this.tbKucoin.Size = new System.Drawing.Size(100, 26);
+            this.tbKucoin.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbKucoin;
+        private System.Windows.Forms.TextBox tbBybit;
+        private System.Windows.Forms.TextBox tbBinance;
+    }
+}
+
